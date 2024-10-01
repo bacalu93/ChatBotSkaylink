40f24358a54fc382d8b7db8d1eb76f60cf0c62cd
bacalu93 <andrei.bacalu@skaylink.com>
2024-10-01 16:19:10 +0200

    add  python3 env venv

--- terraform/module_lambda/main.tf
+++ terraform/module_lambda/main.tf
@@ -4,23 +4,32 @@ resource "null_resource" "pip_install" {
   }
 
   provisioner "local-exec" {
-    command = <<-EOT
-      # Create a virtual environment
-      python3 -m venv venv
-      
-      # Use the virtual environment's Python directly without activating
-      ./venv/bin/python -m pip install -r ${var.source_dir}/layer/requirements.txt -t ${var.source_dir}/layer/python/lib/python3.12/site-packages
-      
-      ./venv/bin/python -m pip install pytest boto3
+  command = <<-EOT
+    # List the contents of the layer directory to check for requirements.txt
+    ls -la ${var.source_dir}/layer/
 
-      # Run tests if the tests folder exists
-      if [ -d "${var.source_dir}/tests" ]; then
-        ./venv/bin/python -m pytest ${var.source_dir}/tests
-      else
-        echo "Tests folder does not exist, skipping tests."
-      fi
-    EOT
-  }
+    # Create a virtual environment
+    python3 -m venv venv
+    
+    # Install application dependencies
+    if [ -s ${var.source_dir}/layer/requirements.txt ]; then
+      ./venv/bin/python -m pip install -r ${var.source_dir}/layer/requirements.txt --no-cache-dir -t ${var.source_dir}/layer/python/lib/python3.12/site-packages
+    else
+      echo "requirements.txt not found!"
+    fi
+
+    # Install pytest and other testing dependencies
+    ./venv/bin/python -m pip install pytest boto3
+
+    # Run tests if the tests folder exists
+    if [ -d "${var.source_dir}/tests" ]; then
+      ./venv/bin/python -m pytest ${var.source_dir}/tests
+    else
+      echo "Tests folder does not exist, skipping tests."
+    fi
+  EOT
+}
+
 }
 
 
